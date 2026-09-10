3aaad328dbf6482d82fbd35fb2de3532dd861103
christianGlez100 <christian.glez.100@gmail.com>
2026-09-09 20:04:37 -0600

    fix

--- shared/src/commonMain/kotlin/com/sesi/astralia/data/datasource/ContentDataSource.kt
+++ shared/src/commonMain/kotlin/com/sesi/astralia/data/datasource/ContentDataSource.kt
@@ -1,21 +1,30 @@
 package com.sesi.astralia.data.datasource
 
+import com.sesi.astralia.data.model.ContentComplete
+import com.sesi.astralia.data.model.toDomain
 import com.sesi.astralia.domain.dto.ContentCompleteDto
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
 import io.github.jan.supabase.postgrest.query.Columns
 
 class ContentDataSource(private val supabaseClient: SupabaseClient) {
-    suspend fun getContentBySubCategory(): ContentCompleteDto {
+    suspend fun getContentBySubCategory(subCategoryId: Long): List<ContentCompleteDto> {
         return supabaseClient.from("content").select(
             Columns.raw(
                 """
                 id,
                 name,
                 description,
-                contentType:contentType(*)
+                contentType(*)
             """.trimIndent()
             )
-        ).decodeAs<ContentCompleteDto>()
+        ){
+            filter{
+                eq("subCategoryId", subCategoryId)
+            }
+        }.decodeAs<List<ContentComplete>>()
+            .map {
+                it.toDomain()
+            }
     }
 }
